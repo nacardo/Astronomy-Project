a0f42b608912a25a62d67475f383873a1258e25d
Nick Cardoza <48460641+nacardo@users.noreply.github.com>
2021-06-23 19:03:31 -0500

    No change to code. Added nicer spacing.

--- CCdiagram.py
+++ CCdiagram.py
@@ -14,9 +14,11 @@ band_z = []
 
 def sdss_mod(x): #product of mod and sys lines
     return f_linear(x) * s_linear(x) 
+
 models = glob.glob('*Gyr')
 sdss = sorted(glob.glob('sys.*'))
 sys_count = 0 
+
 for file in sdss:
     file = np.loadtxt(file)
     sys_x = file[:,0]
@@ -24,7 +26,7 @@ for file in sdss:
     s_linear = interp1d(sys_x,sys_y,fill_value='extrapolate')
     #iterate over all model spectra 
     sys_count += 1 
-    #print(sys_count) #used to make sure all sdss files were iterated over
+    
     for model in models:
         model = np.loadtxt(model)
         x = model[:,0]
@@ -45,6 +47,7 @@ for file in sdss:
             band_u.append(magnitude)
         elif sys_count == 5:
             band_z.append(magnitude)
+            
 #subtract band arrays to obtain data for color-color diagram            
 xmain = list(np.array(band_r) - np.array(band_g)) #choose desired bands
 ymain = list(np.array(band_i) - np.array(band_r)) #choose desired bands  
